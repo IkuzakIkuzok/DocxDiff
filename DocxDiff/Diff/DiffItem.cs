ce3b7f7671ed5c0e67cd8e42118fb0a593a36aaf
ikuzak <k.kohzuki@gmail.com>
2023-03-01 22:43:42 +0900

    差分検出機能を実装

---
+++ DocxDiff/Diff/DiffItem.cs
@@ -0,0 +1,30 @@
+﻿
+// (c) 2023 Kazuki KOHZUKI
+
+namespace DocxDiff.Diff;
+
+internal sealed class DiffItem
+{
+    required internal int Start1 { get; init; }
+    required internal int Start2 { get; init; }
+
+    required internal int Deletion1 { get; init; }
+    required internal int Addition2 { get; init; }
+
+    internal DiffItem() { }
+
+    private static bool ContainsPosition(int position, int start, int length)
+    {
+        if (length == 0) return false;
+        if (position < start) return false;
+        if (position >= start + length) return false;
+        return true;
+    } // private static bool ContainsPosition (int, int, int)
+
+    internal bool IsDeleted(int position)
+        => ContainsPosition(position, this.Start1, this.Deletion1);
+
+    internal bool IsAdded(int position)
+        => ContainsPosition(position, this.Start2, this.Addition2);
+} // internal sealed class DiffItem
+
---
+++ DocxDiff/Diff/Point.cs
@@ -0,0 +1,10 @@
+﻿
+// (c) 2023 kazuki KOHZUKI
+
+namespace DocxDiff.Diff;
+
+internal struct Point
+{
+    required internal int X { get; set; }
+    required internal int Y { get; set; }
+} // internal struct Point
